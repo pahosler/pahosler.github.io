20a8ec1b48d1aa3e1873041103a8827b472baa0c
Paul Hosler <pahosler@gmail.com>
2017-09-26 11:06:02 -0500

    init

--- src/App.js
+++ src/App.js
@@ -1,18 +1,51 @@
 import React, { Component } from 'react';
 import logo from './logo.svg';
-import './App.css';
+import $ from 'jquery';
+import { Navbar, Nav } from 'react-bootstrap';
+import Menu from './Menu.js';
+import Home from './Home.js';
+import About from './About.js';
+import Portfolio from './Portfolio';
+import AboutSeparator from './AboutSeparator.js';
+import PortfolioSeparator from './PortfolioSeparator.js';
+import Services from './Services.js';
+import Testimonials from './Testimonials.js';
+import ContactSeparator from './ContactSeparator.js';
 
 class App extends Component {
+  constructor() {
+    super();
+    this.state = {
+      toggleMenu: false
+    };
+
+    this.toggle = {
+      menu: this.handleMenuToggle.bind(this)
+    };
+  }
+
+  toggleMenu() {
+    this.setState({ toggleMenu: !this.state.toggleMenu });
+  }
+
+  handleMenuToggle = () => {
+    $('#menuToggle').toggleClass('active');
+    $('#root').toggleClass('body-push-toleft');
+    $('#theMenu').toggleClass('menu-open');
+  };
+
   render() {
     return (
-      <div className="App">
-        <div className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h2>Welcome to React</h2>
-        </div>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+      <div>
+        <Menu toggle={this.toggle} />
+        <Home />
+        <About />
+        <AboutSeparator />
+        <Portfolio />
+        <PortfolioSeparator />
+        <Services />
+        <Testimonials />
+        <ContactSeparator />
       </div>
     );
   }
--- src/index.js
+++ src/index.js
@@ -3,6 +3,11 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import registerServiceWorker from './registerServiceWorker';
+import 'bootstrap/dist/css/bootstrap.css';
+import 'bootstrap/dist/css/bootstrap-theme.css';
+import './assets/css/style.css';
+import './assets/css/font-awesome.min.css';
+import './assets/js/fancybox/jquery.fancybox.css';
 
 ReactDOM.render(<App />, document.getElementById('root'));
 registerServiceWorker();
